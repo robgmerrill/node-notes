1d4248cd47841e772cadad6b184267d93446eaf7
robgmerrill <robgmerrill@gmail.com>
2016-10-16 17:28:56 -0700

    added npm yargs to access process.argv

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 console.log('starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+const yargs = require('yargs');
+
 const notes = require('./notes.js');
 
+const argv = yargs.argv;
+var command = argv._[0];
 var command = process.argv[2];
 console.log('Command: ', command);
+console.log('Yargs', argv);
 
 if (command === 'add') {
-  console.log('Adding new note');
+  notes.addNote(argv.title, argv.body);
 } else if (command === 'list') {
-  console.log('Listing all notes');
-} else {
+  notes.getAll(argv.title);
+} else if (command === 'read') {
+  notes.getNote();
+} else if (command === 'remove') {
+  notes.removeNote(argv.title);
+  } else {
   console.log('Command not recognized');
 }
--- notes.js
+++ notes.js
@@ -1,13 +1,24 @@
 console.log('Starting notes.js');
 
-module.exports.addNote = () => {
-    console.log('addNote');
-    return 'New note';
-}
+var addNote = (title, body) => {
+  console.log('Adding note', title, body);
+};
 
-module.exports.add = (a, b) => {
-  return a + b;
-}
+var getAll = () => {
+  console.log('getting all notes');
+};
 
-// test content for git
-// more test quotes
+var getNote = (title) => {
+  console.log('Getting note', title);
+};
+
+var removeNote = (title) => {
+  console.log('Removing note', title);
+};
+
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote 
+};
